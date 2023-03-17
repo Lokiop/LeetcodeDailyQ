0be97e5c1663f9d24215dd4b936ba8a1a0e65d4a
Lokiop <87300472+Lokiop@users.noreply.github.com>
2023-03-17 07:42:58 +0530

    17/03/2023

---
+++ 1703_208_ImplementTrie.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node *links[26];
+    bool flag = false;
+
+    bool containsKey(char ch)
+    {
+        return (links[ch - 'a'] != NULL);
+    }
+
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
+    }
+
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie
+{
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (auto &i : word)
+        {
+            if (!node->containsKey(i))
+            {
+                node->put(i, new Node());
+            }
+            node = node->get(i);
+        }
+
+        node->setEnd();
+    }
+
+    bool search(string word)
+    {
+        Node *node = root;
+        for (auto &i : word)
+        {
+            if (!node->containsKey(i))
+            {
+                return false;
+            }
+
+            node = node->get(i);
+        }
+
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix)
+    {
+        Node *node = root;
+        for (auto &i : prefix)
+        {
+            if (!node->containsKey(i))
+                return false;
+            node = node->get(i);
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
