7d06adc01f97c122ab067058f38d5d68300a45ec
Lokiop <87300472+Lokiop@users.noreply.github.com>
2023-03-20 12:50:22 +0530

    20/03/2023

---
+++ 2003_605_CanPlaceFlowers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+            {
+                flowerbed[i] = 1;
+                count++;
+            }
+            if (count >= n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
